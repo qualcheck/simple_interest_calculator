e9ebe4a46f752efb4d4e6428f7452e5f79810d28
Marcus Bird <81392502+qualcheck@users.noreply.github.com>
2021-04-18 19:20:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+//function
+function compute(principal, interestRate, years, interestEarned, d, n, futureDate) {
+//variables
+    var principal = document.getElementById("amountOne").value 
+    var interestRate = document.getElementById("rate").value
+    var years = document.getElementById("noYears").value
+    var interestEarned = principal * interestRate * years *.01 + Number(principal);
+    var d = new Date();
+    var n = d.getFullYear();
+    var futureDate = Number(n) + Number(years); 
+//results
+    document.getElementById("answer1").innerHTML = "If you deposit $" + principal;
+    document.getElementById("answer2").innerHTML = "At a rate of " + interestRate + '%';
+    document.getElementById("answer3").innerHTML = "You'll recieve the amount of $" + interestEarned;
+    document.getElementById("answer4").innerHTML = "In the year " + futureDate;
 }
-        
